45ef2c0f25659d7b7030d924ee941a6264f3fa3e
Satyam Kumar Jha <87626810+satyamkumarjha2002@users.noreply.github.com>
2022-06-09 01:00:04 +0530

    day 15 assignment

--- day15Assignment/src/day15Assignment/question1/Main.java
+++ day15Assignment/src/day15Assignment/question1/Main.java
@@ -1,30 +1,35 @@
 package day15Assignment.question1;
 
+import java.security.GuardedObject;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
-	public void dob() {
+	public Period dob() {
+		Period diff=Period.between(LocalDate.now(), LocalDate.now());
     	Scanner scanner =new Scanner(System.in);
-    	 DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    	 System.out.println("Enter your Date Of Birth");
+    	 DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    	 System.out.println("Enter your Date Of Birth Format(dd/MM/yyyy)");
     	 String dateOfBirth=scanner.next();
     	 try {
     		 LocalDate DOB=LocalDate.parse(dateOfBirth,dateTimeFormatter);
-        	Period diff=Period.between(DOB,LocalDate.now());
+        	 diff=Period.between(DOB,LocalDate.now());
         	if(diff.getYears()<0||diff.getDays()<0||diff.getMonths()<0) {
         		System.out.println("“Date should not be in Future”.");
+        		diff=Period.between(LocalDate.now(), LocalDate.now());
         	}
 		} catch (Exception e) {
 		System.out.println(e.getMessage());
 		}
+    	 return diff;
     }
 
 	public static void main(String[] args) {
         Main main=new Main();
-        main.dob();
+       Period age=   main.dob();
+       System.out.println("User age :- "+age.getYears()+" years "+age.getMonths()+" Month "+age.getDays()+" Day ");
 	}
 
 }
